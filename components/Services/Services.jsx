0133b245779feb5c3cd2e786d045160a91a538f8
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-10-14 17:29:44 -0600

    Add more responsiveness, mb for divs, and change  width for small screens

--- components/Services/Services.jsx
+++ components/Services/Services.jsx
@@ -4,11 +4,13 @@ export const Services = () => {
   return (
     <section className="m-10">
       <h2 className="text-center">Cleaning Services</h2>
-      <div className="pt-6 grid grid-cols-4 gap-2 max-lg:grid-cols-2 max-md:grid-cols-1">
+      <div className="pt-6 grid gap-2 grid-cols-4 max-2xl:grid-cols-2 max-md:grid-cols-1">
         {servicesData.map((data) => (
-          <div key={data.id}>
+          <div key={data.id} className="max-md:mb-6">
             <h3 className="pb-2">{data.title}</h3>
-            <div className="h-[200px] w-[200px] bg-aquamarine-blue">Image</div>
+            <div className="h-[200px] w-[300px] max-sm:w-[240px] bg-aquamarine-blue">
+              Image
+            </div>
             <p>{data.shortDescription}</p>
           </div>
         ))}
